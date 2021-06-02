0e70b75f1ccf1606c2680a585022f6ba00bf781a
Dylan Guma <djguma@live.com>
2021-06-02 15:40:07 -0400

    Changes made from Textastic

--- oldsite/js/app.js
+++ oldsite/js/app.js
@@ -1,21 +1,21 @@
-// JavaScript Document
-
-const logo = document.getElementById('logo');
-const logoContainer = document.querySelector('.logoContainer');
-const body = document.body;
-const navBar = document.getElementById('navBar');
-const mainVideoContainer = document.querySelector('.mainVideoContainer');
-const videoPlayer  = document.querySelector('video');
-
-body.onclick = function(){
-//	logo.classList.add('logoPosition');
-	logoContainer.classList.add('logoPosition');
-	navBar.classList.add('navBarVisibility');
-}
-
-//videoPlayer.play();
-//document.ready(function(){
-//    // Target your .container, .wrapper, .post, etc.
-//    mainVideoContainer.fitVids();
-//  });
-
+// JavaScript Document
+
+const logo = document.getElementById('logo');
+const logoContainer = document.querySelector('.logoContainer');
+const body = document.body;
+const navBar = document.getElementById('navBar');
+const mainVideoContainer = document.querySelector('.mainVideoContainer');
+const videoPlayer  = document.querySelector('video');
+
+body.onclick = function(){
+//	logo.classList.add('logoPosition');
+	logoContainer.classList.add('logoPosition');
+	navBar.classList.add('navBarVisibility');
+}
+
+//videoPlayer.play();
+//document.ready(function(){
+//    // Target your .container, .wrapper, .post, etc.
+//    mainVideoContainer.fitVids();
+//  });
+
